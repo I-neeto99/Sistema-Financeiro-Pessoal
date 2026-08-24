80df64a674eb017c6dc89f34f54bb3ee5a75744f
I.neeto99 <italo.neeto.oliv@outlook.com>
2026-08-24 20:02:03 -0300

    fix: corrige nome da classe Transacao e ajusta campo id para Long

---
+++ ficanceiro/src/main/java/com/italo/financeiro/model/Transacao.java
@@ -0,0 +1,33 @@
+package com.italo.financeiro.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name= "transacoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Transacao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @Column(nullable = false)
+    private BigDecimal valor;
+
+    @Column(nullable = false)
+    private String tipo; //"RECEITA" ou "DESPESA"
+
+    @Column(nullable = false)
+    private LocalDate data;
+}
