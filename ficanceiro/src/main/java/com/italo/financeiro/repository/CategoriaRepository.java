b3126ab73cab45f64ddc9887817829346f4b9d58
I.neeto99 <italo.neeto.oliv@outlook.com>
2026-08-27 17:47:24 -0300

    feat: cria CategoriaRepository extendendo JpaRepository

---
+++ ficanceiro/src/main/java/com/italo/financeiro/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.italo.financeiro.repository;
+
+import com.italo.financeiro.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+}
